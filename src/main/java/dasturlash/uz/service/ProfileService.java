e56a714c68ddb8f0a29ad49f1d8232be6e449321
Qiyomiddin-17 <qiyomiddinjumabekov1@gmail.com>
2026-05-12 12:49:21 +0500

    Update Profile method added

--- src/main/java/dasturlash/uz/service/ProfileService.java
+++ src/main/java/dasturlash/uz/service/ProfileService.java
@@ -1,9 +1,12 @@
 package dasturlash.uz.service;
 
+import dasturlash.uz.dto.RequestDtoForUpdateProfile;
 import dasturlash.uz.entity.Profile;
 import dasturlash.uz.repository.ProfileRepository;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,11 +16,21 @@ public class ProfileService {
     @Autowired
     private ProfileRepository profileRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public Profile getProfileById(Integer id) {
-        Optional<Profile> profile = profileRepository.findById(id);
-        if (profile.isPresent()) {
-            return profile.get();
-        }
-        throw new IllegalArgumentException("Profile with id " + id + " not found");
+        return profileRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Profile with id " + id + " not found"));
+    }
+
+    public Profile updateProfile(Integer id, @Valid RequestDtoForUpdateProfile request) {
+        Profile oldProfile = getProfileById(id);
+
+        oldProfile.setSurname(request.surname());
+        oldProfile.setName(request.name());
+        oldProfile.setPassword(bCryptPasswordEncoder.encode(request.password()));
+        oldProfile.setUsername(request.username());
+        return profileRepository.save(oldProfile);
     }
 }
