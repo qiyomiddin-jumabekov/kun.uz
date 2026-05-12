4a56b843554f5a9643884a9076fad63a994fd37b
Qiyomiddin-17 <qiyomiddinjumabekov1@gmail.com>
2026-05-12 12:02:54 +0500

    existByUsername method added

--- src/main/java/dasturlash/uz/repository/ProfileRepository.java
+++ src/main/java/dasturlash/uz/repository/ProfileRepository.java
@@ -1,7 +1,11 @@
 package dasturlash.uz.repository;
 
 import dasturlash.uz.entity.Profile;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AuthorizationRepository extends CrudRepository<Profile, Integer> {
+public interface ProfileRepository extends CrudRepository<Profile, Integer> {
+
+
+    boolean existsByUsername(String username);
 }
