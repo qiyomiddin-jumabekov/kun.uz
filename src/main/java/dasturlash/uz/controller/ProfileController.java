c65a9c53f0c64b20751c4a68b3528128af036b49
Qiyomiddin-17 <qiyomiddinjumabekov1@gmail.com>
2026-05-12 12:49:15 +0500

    Update Profile method added

--- src/main/java/dasturlash/uz/controller/ProfileController.java
+++ src/main/java/dasturlash/uz/controller/ProfileController.java
@@ -1,15 +1,14 @@
 package dasturlash.uz.controller;
 
+import dasturlash.uz.dto.RequestDtoForUpdateProfile;
 import dasturlash.uz.entity.Profile;
 import dasturlash.uz.service.ProfileService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/profile")
@@ -23,5 +22,12 @@ public class ProfileController {
     public ResponseEntity<Profile> getProfileById(
             @PathVariable @Min(value = 1, message = "Id is not negative nums") Integer id) {
         return ResponseEntity.ok().body(profileService.getProfileById(id));
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Profile> updateProfile(
+            @RequestParam("id") Integer id,
+            @RequestBody @Valid RequestDtoForUpdateProfile request) {
+        return ResponseEntity.ok().body(profileService.updateProfile(id, request));
     }
 }
