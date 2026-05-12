c6372a24a1de1fd0527288b5e79cca72f991a246
Jumabekov-17 <qiyomiddinjumabekov1@gmail.com>
2026-05-12 11:49:12 +0500

    Delete src/main/java/dasturlash/uz/repository/AuthorizationRepository.java

--- src/main/java/dasturlash/uz/repository/AuthorizationRepository.java
+++
@@ -1,7 +0,0 @@
-package dasturlash.uz.repository;
-
-import dasturlash.uz.entity.Profile;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorizationRepository extends CrudRepository<Profile, Integer> {
-}
