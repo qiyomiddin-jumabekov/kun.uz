7fb2fa52d69653cc8d8f1303dadc67b00c2e9247
Qiyomiddin-17 <qiyomiddinjumabekov1@gmail.com>
2026-05-12 12:10:10 +0500

    profile service and repo created

---
+++ src/main/java/dasturlash/uz/controller/ProfileController.java
@@ -0,0 +1,9 @@
+package dasturlash.uz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/profile")
+public class ProfileController {
+}
