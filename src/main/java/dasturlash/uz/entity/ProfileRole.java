9880cbb88329f9d209cd7947371d1677c48eb0a0
Qiyomiddin-17 <qiyomiddinjumabekov1@gmail.com>
2026-05-12 10:56:54 +0500

    Getter and Setter added

--- src/main/java/dasturlash/uz/entity/ProfileRole.java
+++ src/main/java/dasturlash/uz/entity/ProfileRole.java
@@ -2,8 +2,12 @@ package dasturlash.uz.entity;
 
 import dasturlash.uz.enums.ProfileRoles;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class ProfileRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
