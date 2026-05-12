0869c4dee8f39cf4fd6a66208899ba56fc93c930
Qiyomiddin-17 <qiyomiddinjumabekov1@gmail.com>
2026-05-12 12:18:47 +0500

    Get by Id method added for Profile at ProfileService and ProfileController

--- src/main/java/dasturlash/uz/controller/ProfileController.java
+++ src/main/java/dasturlash/uz/controller/ProfileController.java
@@ -1,9 +1,27 @@
 package dasturlash.uz.controller;
 
+import dasturlash.uz.entity.Profile;
+import dasturlash.uz.service.ProfileService;
+import jakarta.validation.constraints.Min;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/profile")
+@Validated
 public class ProfileController {
+
+    @Autowired
+    private ProfileService profileService;
+
+    @GetMapping("/by-id/{id}")
+    public ResponseEntity<Profile> getProfileById(
+            @PathVariable @Min(value = 1, message = "Id is not negative nums") Integer id) {
+        return ResponseEntity.ok().body(profileService.getProfileById(id));
+    }
 }
--- src/main/java/dasturlash/uz/service/ProfileService.java
+++ src/main/java/dasturlash/uz/service/ProfileService.java
@@ -1,7 +1,23 @@
 package dasturlash.uz.service;
 
+import dasturlash.uz.entity.Profile;
+import dasturlash.uz.repository.ProfileRepository;
+import jakarta.validation.constraints.Min;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProfileService {
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    public Profile getProfileById(Integer id) {
+        Optional<Profile> profile = profileRepository.findById(id);
+        if (profile.isPresent()) {
+            return profile.get();
+        }
+        throw new IllegalArgumentException("Profile with id " + id + " not found");
+    }
 }
