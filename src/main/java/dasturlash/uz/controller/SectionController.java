4dfe1e4a8f576f6021adb40b43bf999202611932
Qiyomiddin-17 <qiyomiddinjumabekov1@gmail.com>
2026-05-14 22:17:01 +0500

    Delete Section Method added

--- src/main/java/dasturlash/uz/controller/SectionController.java
+++ src/main/java/dasturlash/uz/controller/SectionController.java
@@ -3,12 +3,15 @@ package dasturlash.uz.controller;
 import dasturlash.uz.dto.section.RequestForSection;
 import dasturlash.uz.service.SectionService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/section")
+@Validated
 public class SectionController {
     @Autowired
     private SectionService sectionService;
@@ -21,5 +24,10 @@ public class SectionController {
     @PutMapping("/{id}")
     public ResponseEntity<String> updateSection(@PathVariable Integer id, @RequestBody @Valid RequestForSection request) {
         return ResponseEntity.ok().body(sectionService.updateSectionById(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteSection(@PathVariable @Min(value = 1, message = "Delete Id has been positive") Integer id) {
+        return ResponseEntity.ok().body(sectionService.deleteSectionById(id));
     }
 }
--- src/main/java/dasturlash/uz/service/SectionService.java
+++ src/main/java/dasturlash/uz/service/SectionService.java
@@ -2,7 +2,9 @@ package dasturlash.uz.service;
 
 import dasturlash.uz.dto.section.RequestForSection;
 import dasturlash.uz.entity.Section;
+import dasturlash.uz.enums.Visible;
 import dasturlash.uz.repository.SectionRepository;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,5 +41,12 @@ public class SectionService {
 
     public Section getSectionById(Integer id) {
         return sectionRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Section not found"));
+    }
+
+    public String deleteSectionById(Integer id) {
+        Section section = getSectionById(id);
+        section.setVisible(Visible.INACTIVE);
+        sectionRepository.save(section);
+        return "Section succesfully deleted";
     }
 }
