3ad29d6b7c32c889b4aa2b8e38aa1a81a10b5bff
Qiyomiddin-17 <qiyomiddinjumabekov1@gmail.com>
2026-06-23 13:46:14 +0500

    m

--- src/main/java/dasturlash/uz/controller/CommentController.java
+++ src/main/java/dasturlash/uz/controller/CommentController.java
@@ -1,14 +1,12 @@
 package dasturlash.uz.controller;
 
 import dasturlash.uz.dto.article.CreateCommentRequest;
+import dasturlash.uz.dto.article.UpdateCommentRequest;
 import dasturlash.uz.service.CommentService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/article-comment")
@@ -21,4 +19,11 @@ public class CommentController {
     public ResponseEntity<?> createComment(@RequestBody @Valid CreateCommentRequest request) {
         return ResponseEntity.ok(commentService.createComment(request));
     }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateComment(@RequestBody @Valid UpdateCommentRequest request) {
+        return ResponseEntity.ok(commentService.updateComment(request));
+    }
+
+
 }
